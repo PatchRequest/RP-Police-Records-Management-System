c5a4588ee77c6f94e16f4bf6caf6f8f876ff1c9b
Daniel <daniriebel12040@gmail.com>
2019-08-05 19:36:48 +0200

    finished Ranks

--- app/Rank.php
+++ app/Rank.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rank extends Model
 {
+    public $timestamps = [];
 
+    protected $fillable = [
+        'name'
+    ];
+
+    public function owner(){
+        return $this->belongsToMany(User::class);
+    }
 
 }
