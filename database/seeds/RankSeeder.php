151fbe9b26002aee3c5de4d8779977677521de76
Daniel <daniriebel12040@gmail.com>
2019-08-05 19:32:23 +0200

    finished login system

---
+++ database/seeds/RankSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('ranks')->insert([
+            'name' => 'Anwärter'
+        ]);
+        DB::table('ranks')->insert([
+            'name' => 'Wachtmeister'
+        ]);
+        DB::table('ranks')->insert([
+            'name' => 'Oberwachtmeister'
+        ]);
+        DB::table('ranks')->insert([
+            'name' => 'Hauptwachtmeister'
+        ]);
+        DB::table('ranks')->insert([
+            'name' => 'Kommisar'
+        ]);
+        DB::table('ranks')->insert([
+            'name' => 'Oberkommisar'
+        ]);
+        DB::table('ranks')->insert([
+            'name' => 'Hauptkommisar'
+        ]);
+    }
+}
