fb3929f58a7dba27868022cf9ace963ec3d88dff
Daniel <daniriebel12040@gmail.com>
2019-08-06 09:55:23 +0200

    Improved Userlist

--- resources/views/User/userlist.blade.php
+++ resources/views/User/userlist.blade.php
@@ -2,8 +2,34 @@
 
 @section('content')
 
-    @foreach($users as $user)
-        {{ $user->username }}
-    @endforeach
-    {{ $users->links() }}
+
+
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">UID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Rang</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($users as $user)
+            <tr>
+                <th scope="row">{{ $user->UID }}</th>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->rank->name }}</td>
+            </tr>
+
+        @endforeach
+
+
+
+        </tbody>
+        {{ $users->links() }}
+    </table>
+
+
+
+
 @endsection
