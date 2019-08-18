6fae72b5857b11600d6d96bfb7957bc0f9295fc4
Daniel <daniriebel12040@gmail.com>
2019-08-18 20:30:01 +0200

    removed bootstrap and added Adminlte

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,46 +1,168 @@
-<!doctype html>
-<html alng="en"  >
+<!DOCTYPE html>
+<html>
 <head>
-    <title>@yield('title','Z01 Zivilpolizei')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title', 'Zivilpolizei')</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('bower_components/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset('bower_components/Ionicons/css/ionicons.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('dist/css/AdminLTE.min.css')}}">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{asset('dist/css/skins/_all-skins.min.css')}}">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="{{asset('bower_components/morris.js/morris.css')}}">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="{{asset('bower_components/jvectormap/jquery-jvectormap.css')}}">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="{{asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
 
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
-    <script>tinymce.init({selector:'textarea'});</script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body  background="{{asset("/images/background.jpg")}}" style="background-attachment: fixed;
-    background-repeat: no-repeat;
-    background-size: cover;" >
-
-
-
+<body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
-    <nav >
+    <header class="main-header">
+        <!-- Logo -->
+        <a  class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini">Z01</span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Zivilpolizei</span>
+        </a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top">
+            <!-- Sidebar toggle button-->
 
-        @include('navbar')
-    </nav>
 
-    <div id="content">
-        <div class="@yield('container','container')"  >
-            <div class="jumbotron mt-3">
-                @include('errors')
-                @yield('content')
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+
+
+
+                    <!-- User Account: style can be found in dropdown.less -->
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="hidden-xs">{{ auth()->user()->username }}</span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <!-- User image -->
+                            <li class="user-header">
+
+                                <p>
+                                    {{ auth()->user()->username }}
+                                    <small>Eingestellt am {{ auth()->user()->created_at->format('d.m.Y') }}</small>
+                                </p>
+                            </li>
+
+
+                            <!-- Menu Footer-->
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="/user/{{ auth()->user()->id }}" class="btn btn-default btn-flat">Profil</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="/logout" class="btn btn-default btn-flat">ausloggen</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
             </div>
-        </div>
+        </nav>
+    </header>
+
+    @include('navbar')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Neuigkeiten
+            </h1>
+
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            @yield('content')
+        </section>
+        <!-- /.content -->
     </div>
 
-</div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
 
-<div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
-    <!-- (Optional) Latest compiled and minified JavaScript translation files -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
+            made with ❤ by Daniel Riebel
+
+
+    </footer>
+
+
+    <!-- Add the sidebar's background. This div must be placed
+         immediately after the control sidebar -->
+
 </div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<!-- Morris.js charts -->
+<script src="{{asset('bower_components/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('bower_components/morris.js/morris.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
+<!-- jvectormap -->
+<script src="{{asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+<script src="{{asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{asset('bower_components/jquery-knob/dist/jquery.knob.min.js')}}"></script>
+<!-- daterangepicker -->
+<script src="{{asset('bower_components/moment/min/moment.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+<!-- datepicker -->
+<script src="{{asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="{{asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
+<!-- Slimscroll -->
+<script src="{{asset(' bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+<!-- FastClick -->
+<script src="{{ asset('bower_components/fastclick/lib/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('dist/js/demo.js') }}"></script>
 </body>
 </html>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,114 +1,122 @@
-@auth
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-        <a class="navbar-brand" href="/">Z01 Zivil-Polizei</a>
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+    {{--
+           <!-- search form -->
+           <form action="#" method="get" class="sidebar-form">
+               <div class="input-group">
+                   <input type="text" name="q" class="form-control" placeholder="Search...">
+                   <span class="input-group-btn">
+                   <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                   </button>
+                 </span>
+               </div>
+           </form>
 
+     --}}
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dokumente
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                        @foreach($documents as $document)
-
-                            <a class="dropdown-item" href="{{ $document->url }}" target="_blank">{{ $document->name }}</a>
-
-                        @endforeach
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">NAVIGATION</li>
 
 
-
-                        @can("manage documents")
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/document/manage">Verwalte Dokumente</a>
-                        @endcan
-
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Bewertungen
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/rating/ask">Anfordern</a>
-                        <a class="dropdown-item" href="/rating/create">Abgeben</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="/rating/forMe">Forderungen an Dich</a>
-                    </div>
-                </li>
-
-
-                @can('manage news')
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        News
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/news/create">Erstellen</a>
-
-                    </div>
-                </li>
-                @endcan
-
-
-                @if(auth()->user()->can('edit permissions') || auth()->user()->can('create users') || auth()->user()->can('show ratings') )
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Admin-Panel
-                        </a>
-
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            @can("create users")
-                            <a class="dropdown-item" href="/user/create">User Erstellen</a>
-                            @endcan
-
-                            <a class="dropdown-item" href="/user">Alle User</a>
-
-                             @can('show ratings')
-                                <a class="dropdown-item" href="/rating">Alle Bewertungen</a>
-                            @endcan
-
-                            @can("edit permissions")
-                            <a class="dropdown-item" href="/permissions">Rechte System</a>
-                            @endcan
-                        </div>
-                    </li>
-
-                @endif
+            <li><a href="/user/{{auth()->user()->id}}"><i class="fa fa-folder-open"></i> <span>Meine Akte</span></a></li>
 
 
 
 
 
+            <li class="treeview">
 
-                <li class="nav-item dropdown ">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ auth()->user()->username }}
-                    </a>
+                <a href="#">
+                    <i class="fa fa-book"></i> <span>Dokumente</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
 
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/user/{{ auth()->user()->id }}"> Mein Profil</a>
-                        <a class="dropdown-item" href="/logout">Log Out</a>
+                </a>
+                <ul class="treeview-menu">
+                    @foreach($documents as $document)
 
-                    </div>
-                </li>
-
-            </div>
-        </div>
-
-    </nav>
-@endauth
+                        <li><a href="{{ $document->url }}" target="_blank"><i class="fa fa-circle-o"></i> {{ $document->name }}</a></li>
 
 
+                    @endforeach
+
+                    @can("manage documents")
+                            <li>
+                                <a class="dropdown-item" href="/document/manage" class="fa fa-circle-o">Verwalte Dokumente</a>
+                            </li>
+
+                    @endcan
+
+                </ul>
+            </li>
+
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-edit"></i>
+                    <span>Bewertungen</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+
+                    <li><a href="/rating/ask"><i class="fa fa-binoculars"></i> Anfordern</a></li>
+                    <li><a href="/rating/create"><i class="fa fa-envelope-o"></i> Abgeben</a></li>
+                    <li><a href="/rating/forMe"><i class="fa fa-bell-o"></i> Forderungen an Dich</a></li>
+
+                </ul>
+            </li>
+
+
+
+            @can('manage news')
+            <li><a href="/news/create"><i class="fa fa-laptop"></i> <span>News verwalten</span></a></li>
+
+
+
+            @endcan
+
+            @if(auth()->user()->can('edit permissions') || auth()->user()->can('create users') || auth()->user()->can('show ratings') )
+
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-database"></i> <span>Admin-Panel</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+
+
+
+
+                    @can("create users")
+                    <li><a href="/user/create"><i class="fa fa-user-plus"></i> User Erstellen</a></li>
+                    @endcan
+                    @can('show ratings')
+                    <li><a href="/rating"><i class="fa fa-trophy"></i> Alle Bewertungen</a></li>
+                    @endcan
+                    @can("edit permissions")
+                    <li><a href="/permissions"><i class="fa fa-eye"></i> Rechte System</a></li>
+                    @endcan
+                    <li><a href="/user"><i class="fa fa-users"></i> Alle User</a></li>
+                </ul>
+            </li>
+
+
+            @endif
+
+
+
+
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
